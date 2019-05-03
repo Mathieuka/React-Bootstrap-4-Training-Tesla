f204250268fe74b3da15e1ea850b7aa00e247884
KAHLAOUI Mathieu <mathieu.kahlaoui@oscaro.com>
2019-05-03 10:44:13 +0200

    footer done

--- src/lib/footer.js
+++ src/lib/footer.js
@@ -6,8 +6,8 @@ import React from 'react';
 
 const Footer = (props) => {
     return ( 
-        <div className='p-3 bg-light'>
-                <div>2019 © Tesla</div>
+        <div className=' p-3 bg-light'>
+              2019 © Tesla
         </div>
      );
 }
